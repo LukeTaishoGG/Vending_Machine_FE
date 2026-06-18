ee8f7b534bc849a9401b6db57b47da885f4cee92
Taisho <luke.wood.et@gmail.com>
2026-06-18 18:35:58 +0900

    パス修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { AppShell } from '@/components/layout/AppShell'
+import { AppShell } from '@/components/Layout/AppShell'
 import { Providers } from './providers'
 import './globals.css'
 import type { Metadata } from 'next'
